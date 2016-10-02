c424cc8af96e88b7100bbfa15d0c77aaf229d6a0
Marion Schleifer <marion.schleifer@simplificator.com>
2016-10-02 15:42:14 +0200

    Add star rating Seed data in English Fix style

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -22,24 +22,24 @@ namespace NotesApp.Models
                 context.Notes.AddRange(
                     new Note
                     {
-                        Title = "Oma abholen",
-                        Content = "17:25 Gleis 15, achtung läuft nicht gut, pünktlich da sein",
+                        Title = "Pick up grandma",
+                        Content = "17:25 platform 15, be on time!",
                         FinishDate = DateTime.Parse("2016-09-26 17:25"),
                         Finished = false,
                         Importance = 5
                     },
                     new Note
                     {
-                        Title = "Mini Projekt Internet Technologien",
-                        Content = "JunitTests? Validierung? DatePicker?",
+                        Title = "Mini project internet technologies",
+                        Content = "JunitTests? Validation? DatePicker?",
                         FinishDate = DateTime.Parse("2016-10-09 00:00"),
                         Finished = false,
                         Importance = 4
                     },
                     new Note
                     {
-                        Title = "Einkaufen",
-                        Content = "Milch, Zopf, Marmelade, Salat, Kiwi",
+                        Title = "Shopping",
+                        Content = "Milk, bread, marmelade, salad, kiwi",
                         FinishDate = DateTime.Parse("2016-09-17 18:00"),
                         Finished = true,
                         FinishedDate = DateTime.Parse("2016-09-25 10:00"),
@@ -47,8 +47,9 @@ namespace NotesApp.Models
                     },
                     new Note
                     {
-                        Title = "Geburtstagsgeschenk Mella",
-                        Content = "Steckenpferd? Andere Idee?",
+                        Title = "Birthday present Mella",
+                        Content = "IPad? Other idea?",
+                        FinishDate = DateTime.Parse("2016-09-19 17:00"),
                         Finished = true,
                         FinishedDate = DateTime.Parse("2016-09-24 10:00"),
                         Importance = 3
